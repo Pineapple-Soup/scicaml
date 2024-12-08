89bdbbe42042b9aea1719423501760046f1e984a
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-08 14:11:28 -0800

    build: Create Vector type

---
+++ lib/core/vector.ml
@@ -0,0 +1,12 @@
+type t = float array
+
+let create n = Array.make n 0.0
+
+let create n value  = Array.make n value
+
+let size v = Array.length v
+
+let add v1 v2 = 
+  if Array.length v1 <> Array.length v2 then
+    raise (Invalid_argument "Vector.add: vectors must have the same size");
+  Array.map2 ( +. ) v1 v2
