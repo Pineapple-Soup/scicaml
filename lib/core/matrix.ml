9356061b970790ff434446eba26f13cebc3f252f
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-08 18:12:00 -0800

    build: handle Matrix creation with negative dimensions

--- lib/core/matrix.ml
+++ lib/core/matrix.ml
@@ -1,6 +1,9 @@
 type t = float array array
 
-let create n m value = Array.make_matrix n m value
+let create n m value = 
+  if n < 0 || m < 0 then
+    raise (Invalid_argument "Matrix.create: size must be non-negative");
+  Array.make_matrix n m value
 
 let zeroes n m = create n m 0.0
 
