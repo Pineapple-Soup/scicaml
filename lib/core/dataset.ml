8d7a569a01f6cde99e6d9f54158d5545f535c65f
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-10 19:36:25 -0800

    build: add Dataset from_csv

--- lib/core/dataset.ml
+++ lib/core/dataset.ml
@@ -20,3 +20,18 @@ let standardize dataset =
   let new_cols = Array.map (fun row -> Vector.scale (Vector.sub row (Vector.create (Vector.size row) (Vector.mean row))) (1. /. Vector.std row)) transposed_features in
   let new_features = Matrix.transpose new_cols in
   create new_features dataset.labels
+
+let from_csv file label_col =
+  let data = Csv.load file in
+  let rows = List.length data in
+  let cols = List.length (List.hd data) in
+  let features = Matrix.create rows cols 0.0 in
+  let labels = Vector.create rows 0.0 in
+  List.iteri (fun i row ->
+    List.iteri (fun j elem ->
+      if j = label_col then
+        labels.(i) <- float_of_string elem
+      else
+        features.(i).(j) <- float_of_string elem
+    ) row) data;
+  create (Matrix.drop features label_col 1) labels
